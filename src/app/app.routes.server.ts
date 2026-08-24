330fa813a42efd8da3ff6b78fb7bbef84542e671
Kassya <kassyabrito2014@gmail.com>
2026-08-24 20:36:29 -0300

    ACABOO 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'jogos/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'jogos/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
