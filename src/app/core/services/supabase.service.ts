26979cc238b4dc27c30cb9477fab694278b08254
Kauany Vitoria Lima Moreira <eng.kmoreira@gmail.com>
2026-09-21 11:35:31 -0300

    git commit -m "chore: add supabase dependency"

---
+++ src/app/core/services/supabase.service.ts
@@ -0,0 +1,45 @@
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Injectable,
+  PLATFORM_ID,
+  inject,
+} from '@angular/core';
+
+import {
+  createClient,
+  SupabaseClient,
+} from '@supabase/supabase-js';
+
+import { environment } from '../../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+
+  private readonly isBrowser =
+    isPlatformBrowser(
+      inject(PLATFORM_ID),
+    );
+
+
+  readonly client: SupabaseClient =
+    createClient(
+      environment.supabaseUrl,
+      environment.supabasePublishableKey,
+      {
+        auth: {
+          persistSession:
+            this.isBrowser,
+
+          autoRefreshToken:
+            this.isBrowser,
+
+          detectSessionInUrl:
+            this.isBrowser,
+        },
+      },
+    );
+
+}
